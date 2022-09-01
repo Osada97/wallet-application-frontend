0e8ccb742961817385d593d0a6cb9e208bac898c
Osada97 <osadamanohara55@gmail.com>
2022-09-01 23:19:52 +0530

    added image and finalized styles of home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,11 @@
 import React from "react";
+import "../assets/css/home.css";
+import pen from "../assets/img/pen.svg";
+import trash from "../assets/img/trash.svg";
 
 function Home() {
   return (
-    <div>
+    <div className="home-pg">
       <div className="top-row">
         <div className="category-sec">
           <select name="category" id="category">
@@ -24,7 +27,7 @@ function Home() {
             <h1>Spent at keels</h1>
             <p>20/10/2022</p>
           </div>
-          <p>
+          <p className="description">
             Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rerum
             consequatur ipsa deleniti. Corrupti maxime placeat nam sint
             quibusdam natus{" "}
@@ -32,8 +35,54 @@ function Home() {
           <h3>HOUSEHOLD</h3>
           <h4>1000 LKR</h4>
           <div className="option-row">
-            <button>edit</button>
-            <button>delete</button>
+            <button>
+              <img src={pen} alt="pen" />
+            </button>
+            <button>
+              <img src={trash} alt="trash" />
+            </button>
+          </div>
+        </div>
+        <div className="card">
+          <div className="card-row">
+            <h1>Spent at keels</h1>
+            <p>20/10/2022</p>
+          </div>
+          <p className="description">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rerum
+            consequatur ipsa deleniti. Corrupti maxime placeat nam sint
+            quibusdam natus{" "}
+          </p>
+          <h3>HOUSEHOLD</h3>
+          <h4>1000 LKR</h4>
+          <div className="option-row">
+            <button>
+              <img src={pen} alt="pen" />
+            </button>
+            <button>
+              <img src={trash} alt="trash" />
+            </button>
+          </div>
+        </div>
+        <div className="card">
+          <div className="card-row">
+            <h1>Spent at keels</h1>
+            <p>20/10/2022</p>
+          </div>
+          <p className="description">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rerum
+            consequatur ipsa deleniti. Corrupti maxime placeat nam sint
+            quibusdam natus{" "}
+          </p>
+          <h3>HOUSEHOLD</h3>
+          <h4>1000 LKR</h4>
+          <div className="option-row">
+            <button>
+              <img src={pen} alt="pen" />
+            </button>
+            <button>
+              <img src={trash} alt="trash" />
+            </button>
           </div>
         </div>
       </div>
